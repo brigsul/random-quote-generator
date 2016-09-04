0278480d54aeefdc0c6e9933b6b001ec2ac2fa2a
Brian Sullivan <bri.g.sullivan@gmail.com>
2016-09-04 15:37:12 -0400

    Fixed background colors and added quote API

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
+
+var genQuote = "";
+var genAuthor = "";
+
 $(document).ready(function(){
+
+  $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en"), function(json) {
+    var arr = JSON.parse(response);
+    $('#quote').html("<h2>" + arr + "</h2>");
+
+  }
 
   var colors = ['#e67e22', '#3498db', '#9b59b6', '#e74c3c', '#2c3e50'];
 
-  $('body').css({'background': '' + colors[Math.floor(Math.random() * colors.length)]});
+  var primColor = colors[Math.floor(Math.random() * colors.length)];
+
+  $('body').css({'background': '' + primColor});
+  $('#quotebox').css({'color': '' + primColor});
+  $('#creditbox').css({'color': '' + primColor});
+  $('button').css({'background': '' + primColor});
+
+  $("#newQuote").on("click", function() {
+      $('#quote').html("<h2>lakdjf;laskdjf;asldkfj</h2>");
+  });
+
 
 });
