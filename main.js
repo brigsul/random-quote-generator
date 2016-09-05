90921624341d14e081da64f8f6307c5a2298e1ea
Brian Sullivan <bri.g.sullivan@gmail.com>
2016-09-04 20:15:03 -0400

    everything but twitter integration

--- main.js
+++ main.js
@@ -1,26 +1,50 @@
 
-var genQuote = "";
+var genQuote = {};
 var genAuthor = "";
+var tweetquote = "";
 
-$(document).ready(function(){
+function fetchQuote(){
+  $.ajax({
+    type:"GET",
+    dataType:"json",
+    url:"http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en",
+    success:function(response){
+      console.log(response);
+      $('#quote').html(' " ' + response.quoteText);
+      tweetquote = "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" + response.quoteText.replace(/ /g,'');
+      console.log(tweetquote);
+      document.getElementById("tweetthis").href = tweetquote;
+      if (response.quoteAuthor == ''){
+        $('#author').html(' - Unknown');
+      }
+      else {
+        $('#author').html(' - ' + response.quoteAuthor);
+      }
+    }
+  });
+}
 
-  $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en"), function(json) {
-    var arr = JSON.parse(response);
-    $('#quote').html("<h2>" + arr + "</h2>");
+var colors = ['#e67e22', '#3498db', '#9b59b6', '#e74c3c', '#2c3e50'];
 
-  }
+var primColor = colors[Math.floor(Math.random() * colors.length)];
 
-  var colors = ['#e67e22', '#3498db', '#9b59b6', '#e74c3c', '#2c3e50'];
-
-  var primColor = colors[Math.floor(Math.random() * colors.length)];
-
+function newColor(){
+  primColor = colors[Math.floor(Math.random() * colors.length)];
   $('body').css({'background': '' + primColor});
   $('#quotebox').css({'color': '' + primColor});
   $('#creditbox').css({'color': '' + primColor});
   $('button').css({'background': '' + primColor});
+}
+
+$(document).ready(function(){
+
+  newColor();
+  fetchQuote();
 
   $("#newQuote").on("click", function() {
-      $('#quote').html("<h2>lakdjf;laskdjf;asldkfj</h2>");
+    newColor();
+    fetchQuote();
+
   });
 
 
